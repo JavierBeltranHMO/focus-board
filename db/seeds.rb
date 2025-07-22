c9e037e6522e49c4b6e3e9db258c0e24af26b531
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-07-22 11:40:19 -0700

    validates, associations and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+list = List.create(name: 'TODO')
+list.tasks.create(name: 'Wash the dishes')
+list.tasks.create(name: 'Drink water')
+list.tasks.create(name: 'Make food')
+
+list = List.create(name: 'In Progress')
+list = List.create(name: 'Done')
