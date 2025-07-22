375c4cc54789cc27f8d4bafdaa6c6f74e9c5804f
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-07-22 11:34:36 -0700

    create tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_22_183224) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_22_183410) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -28,6 +28,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_22_183224) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.bigint "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_tasks_on_list_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -41,4 +49,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_22_183224) do
   end
 
   add_foreign_key "boards", "users"
+  add_foreign_key "tasks", "lists"
 end
