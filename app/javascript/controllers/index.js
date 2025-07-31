a6a410ca35d66e11019363c64bf88ac99329d3d8
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-07-31 16:23:24 -0700

    troubleshooting issues with firefox

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,3 +1,4 @@
+//app/javascript/controllers/index
 import { Application } from "@hotwired/stimulus";
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
+# config/importmap.rb
+
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
