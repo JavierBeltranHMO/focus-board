14eaaac37e4d06e68b0111c707e6c1bd715732f6
Javier Beltran <170134446+JavierBeltranHMO@users.noreply.github.com>
2025-08-05 11:23:13 -0700

    added seeds

--- start.sh
+++ start.sh
@@ -1,13 +1,12 @@
 set -e
 
-echo "Checking Rails master key..."
 if [ -z "$RAILS_MASTER_KEY" ]; then
   echo " Missing RAILS_MASTER_KEY"
   exit 1
 fi
 
-echo "Running DB migrations..."
 bundle exec rails db:migrate
 
-echo "Starting Puma..."
+bundle exec rails db:seed 
+
 bundle exec puma -C config/puma.rb
